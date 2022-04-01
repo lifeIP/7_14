51802082344539bf2def75eee9c597a8c8b81c90
lifeIP <97748864+lifeIP@users.noreply.github.com>
2022-04-01 23:40:14 +0700

    +++++

--- Project59/Source.cpp
+++ Project59/Source.cpp
@@ -13,16 +13,16 @@ int main()
 {
 	setlocale(LC_ALL, "RUS");
 	float y, z;
-	cout << "Ââåäèòå y: ";
+	cout << "y: ";
 	cin >> y;
-	cout << "Ââåäèòå z: ";
+	cout << "z: ";
 	cin >> z;
-	char res[100];
+	char res[250];
 	convert(y, z, res);
 	cout << res << endl;
-	char* str1 = new char[100];
-	char* str2 = new char[110];
-	strcpy(str1, "OLEG BALDA");
+	char* str1 = new char[250];
+	char* str2 = new char[250];
+	strcpy(str1, "Этому миру не хватает героя, но это не ты!");
 	findANDreplace(str1, str2);
 	cout << str1 << endl;
 	cout << str2 << endl;
@@ -36,51 +36,39 @@ void convert(float y, float z, char* str)
 }
 
 void findANDreplace(char* str_l, char* resault_l) {
-	memset(resault_l, 0, sizeof(char) * 100);
-
-	char exchange[] = " A-A-A ";
-	int counter = 0;
-
+	memset(resault_l, 0, sizeof(char) * 250);
+	char str1[250] = {};
 	char str[250] = {};
+
 	for (short i = 0; i < strlen(str_l); i++) {
 		str[i] = str_l[i];
 	}
-
-	int i = 0;
-
-	int address = strcspn(str, "@");
-	char str1[250] = {};
-	char str2[250] = {};
-
-	for (int i = 0; i < address; i++) {
-		str1[i] = str[i];
-	}
-
-	for (int i = address + 1, ia = 0; i < strlen(str); i++, ia++) {
-		str2[ia] = str[i];
-	}
-
-	memset(str, 0, sizeof(char) * 250);
-
+	char str2[5] = {};
 	int iii = 0;
-	char str_t[100] = {};
-	char str_t2[100] = {};
-	int pass = 0;
-	int sz = 0;
-
-	for (; i < strlen(str1); ) {
-		strncpy(str, i + str1, 50);
-		iii = strcspn(str, "A");
-		strncpy(str_t, i + str, iii);
-		i = i + iii + 1;
-		strncpy(sz + str_t2, str, iii);
-		if (str1[i - 1] == 'A') {
-			strcat(str_t2, exchange);
-			counter++;
+	int was_iii = 0;
+	int start = 0;
+	int end = 0;
+	for (int i = 0; i < strlen(str_l); i++) {
+		strncpy(str, i + str_l, 50);
+		iii = strcspn(str, " ");
+		i = i + iii +1;
+		if (was_iii==0){
+			start = 0;
+			end = i;
 		}
-		sz = strlen(str_t2);
+		else {
+			start = was_iii;
+			end = i;
+		}
+		was_iii = i;
+		int size_word = 0;
+		size_word = end - start;
+		sprintf(str2, "%d ", size_word-1);
+		strncpy(strlen(str1) + str1, start+str_l, size_word);
+		strcat(str1, str2);
 	}
-	for (int i = 0; i < strlen(str_t2); i++) {
-		resault_l[i] = str_t2[i];
+	cout << str1 << endl;
+	for (int i = 0; i < strlen(str1); i++) {
+		resault_l[i] = str1[i];
 	}
 }
